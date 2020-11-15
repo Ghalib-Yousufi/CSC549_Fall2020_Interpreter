d105aa221b67e6e0b416ce259639dac65f72e046
Ghalib Yousufi <mghalibyousufi@gmail.com>
2020-11-15 03:18:05 -0600

    Added Do Math Functionality

--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,15 @@
-import Parser.Parser;
+import Parser.*;
 
 public class Driver 
 {
 
 	public static void main(String[] args) 
 	{
+		
 		Parser.parse("input.spyder");
 		//Parser.display();
 		Interpreter.SpyderInterpreter.interpret(Parser.getParsedStatements());
 		Interpreter.SpyderInterpreter.displayResults();
+		
 	}
 }
--- src/Interpreter/SpyderInterpreter.java
+++ src/Interpreter/SpyderInterpreter.java
@@ -25,25 +25,68 @@ public class SpyderInterpreter
 		{
 			if(s instanceof RememberStatement)
 			{
-				//interpret a remember statement
 				SpyderInterpreter.interpretRememberStatement((RememberStatement)s);
-			}
-			else if(s instanceof ResolveStatement)
-			{
-				SpyderInterpreter.interpretResolveStatement((ResolveStatement)s);
-				
 			}
 		}
 	}
 	
-	private static void interpretRememberStatement(RememberStatement rs)
+	private static int getExpressionValue(Expression e)
 	{
-		SpyderInterpreter.theEnv.addVariable(rs.getName(), rs.getIntValue());
-		SpyderInterpreter.theOutput.add("<HIDDEN> Added " + rs.getName() + " = " + rs.getIntValue() + " to the variable environment.");
+		if(e instanceof ResolveExpression)
+		{
+			return SpyderInterpreter.interpretResolveExpression((ResolveExpression)e);
+		}
+		else if (e instanceof DoMathStatement)
+		{
+			return SpyderInterpreter.interpretDoMathExpression((DoMathStatement)e);
+		}
+		throw new RuntimeException("Not a known expression type: " + e.toString());
 	}
 	
-	private static void interpretResolveStatement(ResolveStatement rs)
+	private static void interpretRememberStatement(RememberStatement rs)
 	{
-		SpyderInterpreter.theOutput.add("<HIDDEN> Variable " + rs.getName() + " resolved.");
+		Expression valueExpression = rs.getValueExpression();
+		int value = SpyderInterpreter.getExpressionValue(valueExpression);
+		SpyderInterpreter.theEnv.addVariable(rs.getName(), value);
+		SpyderInterpreter.theOutput.add("<HIDDEN> Added " + rs.getName() + " = " + rs.getValue() + " to the variable environment.");
+	}
+	
+	private static int interpretDoMathExpression(DoMathStatement dms)
+	{
+		int p1 = SpyderInterpreter.interpretResolveExpression((ResolveExpression)dms.e1);
+		int p2 = SpyderInterpreter.interpretResolveExpression((ResolveExpression)dms.e2);
+		return DoMathStatement.doMath(p1, p2, dms.op);
+	}
+	
+	private static int interpretResolveExpression(ResolveExpression rs)
+	{
+		
+		if (isInteger(rs.getName()))
+		{
+			return Integer.parseInt(rs.getName());
+		}
+		else {
+			try
+			{
+				return SpyderInterpreter.theEnv.getValue(rs.getName());
+			}
+			catch(Exception e)
+			{
+				throw new RuntimeException("Variable " + rs.getName() + " NOT FOUND!");
+			}
+		}
+		
+	}
+
+	public static Boolean isInteger(String s)
+	{
+		for(int i = 0; i < s.length(); i++)
+		{
+			if(!Character.isDigit(s.charAt(i)))
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 }
